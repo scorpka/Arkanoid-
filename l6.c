5496392ff479812b79262eb4b869c5904163085a
scorpka <ylltrabott@mail.ru>
2021-10-09 02:39:55 +0300

    v1.01e added finalImage,fixed some bugs,added retry

--- l6.c
+++ l6.c
@@ -1,14 +1,19 @@
 #include<SDL2/SDL.h>
 #include<SDL2/SDL_ttf.h>
 #include<SDL2/SDL_image.h>
+#define WIDTH 800
+#define HEIGHT 600
 
-void wallDraw(SDL_Renderer* gRenderer,int *wallExist, int *x, int *y,int *scoreNumber, int i)
+void wallDraw(SDL_Renderer* gRenderer,int *wallExist, int *x, int *y,int *scoreNumber, int i,int j)
 {
-  
-  if (wallExist[i] == 0) return;
+ //TODO two basis massive 
+ // if (wallExist[i] == 0) return;
  
- SDL_Rect wall1 = {400-(i*90),100,82,38};
-if ( ((*x == wall1.x || *x+1 == wall1.x)  &&  *y >= wall1.y && *y <= wall1.y+wall1.h)  || (*x == wall1.x + wall1.w &&  *y -100 >= wall1.y && *y -100 <= wall1.y+wall1.h)) {
+ SDL_Rect wall1 = {700-(i*80),100-(j*30),64,22};
+  if (j == 1) i = i+9;
+
+if ( ((*x == wall1.x || *x+1 == wall1.x)  &&  *y >= wall1.y && *y <= wall1.y+wall1.h)  || (*x == wall1.x + wall1.w &&  *y  >= wall1.y && *y  <= wall1.y+wall1.h)) {
+
     if(wallExist[i] !=0) (*scoreNumber)++; 
 
     wallExist[i] = 0;
@@ -24,32 +29,56 @@ if ( ((*x == wall1.x || *x+1 == wall1.x)  &&  *y >= wall1.y && *y <= wall1.y+wal
  
 }
 
-void pixelDraw(SDL_Rect pixelPos,SDL_Rect brickRect,SDL_Renderer* gRenderer,int *x,int *y,int *polX,int *polY,SDL_Texture* dotTexture)
+void pixelDraw(SDL_Rect pixelPos,SDL_Rect brickRect,SDL_Renderer* gRenderer,int *x,int *y,int *polX,int *polY,SDL_Texture* dotTexture, SDL_Texture* redTexture, SDL_Texture* greenTexture, SDL_Texture* blueTexture)
 {
+  SDL_Texture* RGB_Texture = NULL;
    if (*polX == 0) *x = *x+1;
    if (*polY == 0) *y = *y+1;
-   if ( *x == 639){ *polX =1; }else if(*x == 1){*polX = 0;};
-   if ( *y == 479){ *polY =1; }else if(*y == 1){*polY = 0;};
-   if ( ((*x == brickRect.x || *x+1 == brickRect.x)  &&  *y >= brickRect.y && *y <= brickRect.y+brickRect.h)  || (*x == brickRect.x + brickRect.w &&  *y -100 >= brickRect.y && *y -100 <= brickRect.y+brickRect.h)){ *polX = !(*polX); if(*x==brickRect.x  || *x == brickRect.x - 1){*x = *x-5;}} 
+   if ( *x == WIDTH - 1){ *polX =1; }else if(*x == 1){*polX = 0;};
+   if ( *y == HEIGHT - 1){ *polY =1; }else if(*y == 1){*polY = 0;};
+   //collision
+   if ( ((*x == brickRect.x || *x+1 == brickRect.x)  &&  *y >= brickRect.y && *y <= brickRect.y+brickRect.h)  || ((*x == brickRect.x + brickRect.w ||(*x-1 == brickRect.x + brickRect.w))  &&  *y  >= brickRect.y && *y  <= brickRect.y+brickRect.h)){ *polX = !(*polX); if(*x==brickRect.x  || *x == brickRect.x - 1){*x = *x-5;}else if (*x==brickRect.x+brickRect.w  || *x-1 == brickRect.x+brickRect.w){*x = *x +5;}} 
 
-   if (( *y == brickRect.y &&  *x >= brickRect.x && *x <= brickRect.x+brickRect.w)||( *y == brickRect.y + brickRect.h &&  *x +1 >= brickRect.x && *x +1 <= brickRect.x+brickRect.w)) *polY = !*polY; 
+   if (( (*y == brickRect.y || *y-1 == brickRect.y) && *x >= brickRect.x && *x <= brickRect.x+brickRect.w) || ((*y == brickRect.y + brickRect.h || (*y-1 ==brickRect.y + brickRect.h)) && *x >= brickRect.x && *x <= brickRect.x+brickRect.w)){ *polY = !*polY; if(*y==brickRect.y  || *y == brickRect.y - 1){*y = *y-5;}else if(*y==brickRect.y+brickRect.h || *y-1 == brickRect.y+brickRect.h){*y =*y+5;}} 
    
    if (*polX == 1) *x = *x-1;
    if (*polY == 1) *y = *y-1;
- if (*x>640) *x = 637; 
- if (*y>480) *y = 477;
+   //fixed bug: throw behind play zone
+ if (*x>WIDTH) *x = WIDTH -3; 
+ if (*y>HEIGHT) *y = HEIGHT -3;
 
-   pixelPos.x = *x;
+  pixelPos.x = *x;
   pixelPos.y = *y;
+   
  SDL_RenderCopy(gRenderer,dotTexture,NULL,&pixelPos); 
- //SDL_RenderDrawPoint(gRenderer,*x,*y);
+  //added colored way after pixel
+ if (*polX == 1) pixelPos.x = *x + 5+(rand()%25);;
+ if (*polY == 1) pixelPos.y = *y + 5+(rand()%25);;
+ if (*polX == 0) pixelPos.x = *x + 5-(rand()%25);;
+ if (*polY == 0) pixelPos.y = *y + 5-(rand()%25);;
+  pixelPos.h = pixelPos.h/3+1;
+  pixelPos.w = pixelPos.w/3+1;
+  switch( rand() %3)
+  {
+    case 0: RGB_Texture = redTexture;
+            break;
+    case 1: RGB_Texture = greenTexture;
+            break;
+    case 2: RGB_Texture = blueTexture;
+            break;
+  }
+  
+ // SDL_SetTextureAlphaMod(redTexture,iRand);
+
+SDL_RenderCopy(gRenderer,RGB_Texture,NULL,&pixelPos); 
+
 }
 
 void brickDraw(SDL_Rect brickRect,int *polBrick,SDL_Renderer* gRenderer,int *b,SDL_Texture *barTexture)
 {
    if (*polBrick == 0) *b  = *b+1;
    if (*polBrick == 1) *b  = *b-1;
-   if (*b >= 580)  *polBrick = 1;
+   if (*b >= (WIDTH-60))  *polBrick = 1;
    if (*b <= 1)  *polBrick = 0;
 
 
@@ -59,35 +88,63 @@ void brickDraw(SDL_Rect brickRect,int *polBrick,SDL_Renderer* gRenderer,int *b,S
 
 void scoreDraw(TTF_Font* scoreFont,SDL_Renderer* gRenderer,int scoreNumber)
 {
- char* scoreText; scoreText = (char*)malloc(3);
- 
-  *scoreText = scoreNumber +'0';
 
+ char* scoreText; scoreText = (char*)malloc(4);
+ 
+  if (scoreNumber >= 10) {  
+    *(scoreText+1) = scoreNumber%10 +'0';
+    *(scoreText) = (scoreNumber/10)%10+'0';
+    *(scoreText+2) = '\0';
+  }else{
+  *scoreText = scoreNumber +'0';
   *(scoreText+1) = '\0';
+  }
  SDL_Color textColor = {255,255,255};
   
  SDL_Surface* scoreSurface = TTF_RenderText_Solid(scoreFont,scoreText,textColor);
- SDL_Rect scoreRect = {600,5,scoreSurface->w,scoreSurface->h}; 
+ SDL_Rect scoreRect = {WIDTH -60,5,scoreSurface->w,scoreSurface->h}; 
  SDL_Texture* scoreTexture = SDL_CreateTextureFromSurface(gRenderer,scoreSurface);
 
  SDL_RenderCopy(gRenderer,scoreTexture,NULL,&scoreRect);
+}
+void finalCreate(int scoreNumber,SDL_Renderer* gRenderer,SDL_Texture* finalTexture,int quit) {
+  if (scoreNumber >= 18){
+        SDL_RenderCopy(gRenderer,finalTexture,NULL,NULL);
+        SDL_RenderPresent(gRenderer);
+        quit  = 1;
+  }
+
+
 }
 
 int main(void) {
  SDL_Init(SDL_INIT_VIDEO);
  TTF_Init();
- SDL_Window* window = SDL_CreateWindow("moving",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,480,SDL_WINDOW_SHOWN); 
+ SDL_Window* window = SDL_CreateWindow("moving",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,WIDTH,HEIGHT,SDL_WINDOW_SHOWN); 
  SDL_Renderer *gRenderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+
  SDL_Surface* barSurface = IMG_Load("sprite1.PNG");
+ SDL_Surface* finalSurface = IMG_Load("youwin.jpg");
  SDL_Surface* dotSurface = SDL_LoadBMP("dot.bmp");
+ SDL_Surface* redSurface = SDL_LoadBMP("red.bmp");
+ SDL_Surface* greenSurface = SDL_LoadBMP("green1.bmp");
+ SDL_Surface* blueSurface = SDL_LoadBMP("blue.bmp");
+
  TTF_Font* scoreFont = TTF_OpenFont("v_DigitalStrip_v1.5.ttf",52);
 
   SDL_SetColorKey(dotSurface,SDL_TRUE,SDL_MapRGB(dotSurface->format,0xFF,0xFF,0xFF));
+  SDL_SetColorKey(redSurface,SDL_TRUE,SDL_MapRGB(redSurface->format,0,0xFF,0xFF));
+  SDL_SetColorKey(greenSurface,SDL_TRUE,SDL_MapRGB(greenSurface->format,0xFF,0xFF,0xFF));
+  SDL_SetColorKey(blueSurface,SDL_TRUE,SDL_MapRGB(blueSurface->format,0,0xFF,0xFF));
+
  SDL_Texture* dotTexture = SDL_CreateTextureFromSurface(gRenderer,dotSurface);
- SDL_Texture* barTexture = SDL_CreateTextureFromSurface(gRenderer,
-barSurface);
+ SDL_Texture* barTexture = SDL_CreateTextureFromSurface(gRenderer,barSurface);
+ SDL_Texture* redTexture = SDL_CreateTextureFromSurface(gRenderer,redSurface);
+ SDL_Texture* greenTexture = SDL_CreateTextureFromSurface(gRenderer, greenSurface);
+ SDL_Texture* blueTexture = SDL_CreateTextureFromSurface(gRenderer,blueSurface);
+ SDL_Texture* finalTexture = SDL_CreateTextureFromSurface(gRenderer,finalSurface);
+
  SDL_SetRenderDrawColor(gRenderer,0,0,0,0xFF);
- //SDL_Rect wall1 = {400,100,82,38};
  SDL_RenderClear(gRenderer); 
  
  SDL_SetRenderDrawColor(gRenderer,0,0xFF,0xFF,0xFF);
@@ -95,7 +152,7 @@ barSurface);
  int x = 20,y=20;
  int polX = 0;int polY=0;
  int polBrick = 0;
- int wallExist[] = {1,1,1,1,1,1,1,1,1,1};
+ int wallExist[]= {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,};
 
  SDL_Rect brickRect = {300,220,80,40};
  SDL_Rect pixelPos = {0,0,20,20};
@@ -109,19 +166,31 @@ barSurface);
    {
       if(e.type == SDL_QUIT) {
            quit = 1;
-      }
-   }
+      }else if(e.type == SDL_KEYDOWN) {
+       if (e.key.keysym.sym == SDLK_1) { 
+          for (int i=0;i<=18;i++){ wallExist[i] = 1;}
+
+          brickRect.x =300;brickRect.y=220;brickRect.w=80;brickRect.h=40;
+          scoreNumber = 0;
+          x = 20; y=20;    
+          pixelPos.x=0;pixelPos.y=0;pixelPos.w=20;pixelPos.h=20;
+      
+      }}}
  SDL_SetRenderDrawColor(gRenderer,0,80,10,10);
  SDL_RenderClear(gRenderer); 
  
  SDL_SetRenderDrawColor(gRenderer,0,0xFF,0xFF,0xFF);
  brickDraw(brickRect, &polBrick,gRenderer,&(brickRect.x),barTexture);
- pixelDraw(pixelPos,brickRect,gRenderer,&x,&y,&polX,&polY,dotTexture); 
- for (int i = 0; i<4;i++) 
+ pixelDraw(pixelPos,brickRect,gRenderer,&x,&y,&polX,&polY,dotTexture,redTexture,greenTexture,blueTexture); 
+
+ for (int i = 0; i<9;i++) 
  {
- wallDraw( gRenderer,wallExist, &x, &y, &scoreNumber,i);
+   for (int j = 0;j<2;j++)
+ wallDraw( gRenderer,wallExist, &x, &y, &scoreNumber,i,j);
  }
+
  scoreDraw(scoreFont,gRenderer,scoreNumber);
+ finalCreate(scoreNumber,gRenderer,finalTexture,quit);
  SDL_RenderPresent(gRenderer);
  SDL_Delay(5); 
 
